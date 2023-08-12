c9298232592733b27ed4f7e4dabc0f4c917e9992
rvdymw <mateuszw.contact@gmail.com>
2023-08-12 16:36:30 +0200

    ratigns and views

--- database/migrations/2023_08_10_123213_create_ratings_table.php
+++ database/migrations/2023_08_10_123213_create_ratings_table.php
@@ -10,16 +10,19 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up()
-{
-    Schema::create('ratings', function (Blueprint $table) {
-        $table->id();
-        $table->unsignedBigInteger('film_id');
-        $table->unsignedInteger('rating');
-        $table->timestamps();
-
-        $table->foreign('film_id')->references('id')->on('films')->onDelete('cascade');
-    });
-}
+    {
+        Schema::create('ratings', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('film_id');
+            $table->unsignedInteger('user_id');
+            $table->unsignedInteger('rating');
+            $table->timestamps();
+    
+            $table->foreign('film_id')->references('id')->on('films')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+    
 
 
     /**
